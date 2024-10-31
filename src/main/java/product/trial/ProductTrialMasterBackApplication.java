299353fe10347d14b4f6edccb66afdecc179fda5
Zakaria haqiq <zakaria.haqiq@orange.com>
2024-10-31 03:02:56 +0100

    first commit

---
+++ src/main/java/product/trial/ProductTrialMasterBackApplication.java
@@ -0,0 +1,13 @@
+package product.trial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductTrialMasterBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductTrialMasterBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/product/trial/ProductTrialMasterBackApplicationTests.java
@@ -0,0 +1,13 @@
+package product.trial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductTrialMasterBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
